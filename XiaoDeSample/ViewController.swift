b141d389662e497b858af63bbd93901b3da15a69
TangXiaoDe <xiaodehappy@foxmail.com>
2020-06-03 13:44:48 +0800

    Initial Commit

---
+++ XiaoDeSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XiaoDeSample
+//
+//  Created by 小唐 on 2020/6/3.
+//  Copyright © 2020 XiaoDeStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
